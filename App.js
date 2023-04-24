8036c79c1c906ae836774f58e9337b8048f7fe22
Raja Kedia <raja.kedia@FC-Raja.Kedia>
2023-04-24 14:49:50 +0530

    added text ad nudge

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { GAMNudge } from "react-native-ad-manager";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.adContainer}>
+        <GAMNudge
+          adProperties={{
+            adIdentity: "MATCH_INFO",
+          }}
+          containerSize={`${Dimensions.get("window").width - 32}x70`}
+          gamContainerStyle={styles.adBox}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  adContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 90,
+  },
+  adBox: {
+    marginTop: 10,
   },
 });
